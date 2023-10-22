648bf6101a796d8da75aeb1b5d881c4b88488dde
Yi Miao <miaoyi321@outlook.com>
2023-10-22 14:10:03 +0100

    start

---
+++ c_mem_pointer/arr1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Arr struct Arr_ 
+
+struct Arr_{
+ 	char d;
+	int a,b,c;
+};
+
+
+int main()
+{
+	char array[3];
+	Arr x;
+	printf("%p, %p\n",&(x.d),&(x.a));
+
+}
